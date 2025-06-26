975535f6e5bf250b43c746935c83bd8ddb51e3fc
Bogdan Caliman <bogdancaliman@192.168.1.10>
2025-06-26 14:27:54 +0300

    feat: added shadcn-ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
+import { Button } from '@/components/ui/button'
 
 function App() {
 
   return (
     <>
     <h1 className="text-3xl font-bold underline text-purple-500 ">    Hello world!  </h1>
+    <div className="flex items-center justify-center h-screen">
+      <div className='w-60 bg-amber-200'>
+
+      <Button className="w-full px-4 py-2">Click me</Button>
+      </div>
+    </div>
     </>
   )
 }
